061c833d62110747082d9d05f1183596a16e8a0e
Sachit Karki <sachitjungkarki@gmail.com>
2021-04-06 09:41:36 +0545

    loading added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, ScrollView, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, ScrollView, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 
 import theme from './CustomProperties/Themes';
@@ -11,30 +11,45 @@ import getResults from './actions/resultsAction';
 export default function App() {
   const [results, setResults] = useState([]);
 
+  const handleRefresh = () => {
+    setResults([]);
+    getResults(setResults);
+  };
+
   useEffect(() => {
     // only on first load
     getResults(setResults);
     const interval = setInterval(() => {
-      getResults(setResults);
+      handleRefresh();
     }, 60 * 60 * 1000); // update in one hour
     return () => {
       clearInterval(interval);
     };
   }, []);
 
+  const Loading = () => (
+    <View style={styles.loading}>
+      <ActivityIndicator size="large" color="#0000ff" />
+    </View>
+  );
+
+  const Body = () => (
+    <ScrollView>
+      {results.map((result) => (
+        <CustomCard
+          key={result.key}
+          title={result.title}
+          content={result.content}
+        />
+      ))}
+    </ScrollView>
+  );
+
   return (
     <PaperProvider theme={theme}>
       <View style = {styles.container}>
-        <TopBar handleRefresh={() => getResults(setResults)}/>
-        <ScrollView>
-          {results.map((result) => (
-            <CustomCard
-              key={result.key}
-              title={result.title}
-              content={result.content}
-            />
-          ))}
-        </ScrollView>
+        <TopBar handleRefresh={handleRefresh}/>
+        { results.length ? <Body/> : <Loading /> }
       </View>
     </PaperProvider>
   );
@@ -43,5 +58,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center"
   },
+  loading: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around"
+  }
 });
